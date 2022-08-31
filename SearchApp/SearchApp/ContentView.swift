4e8996417fc5656642eb226c4de44d69535ad0fd
eunjoo-choi <epcej0020@gmail.com>
2022-08-31 16:14:06 +0900

    프로젝트 생성
    ## 수정 사항
    
    - 앱 프로젝트 생성
       - Unit / UI Test 추가

---
+++ SearchApp/SearchApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SearchApp
+//
+//  Created by 최은주 on 2022/08/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SearchApp/SearchApp/SearchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchAppApp.swift
+//  SearchApp
+//
+//  Created by 최은주 on 2022/08/31.
+//
+
+import SwiftUI
+
+@main
+struct SearchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
