bf082a469c11348ff4d8b8dafec41ee6f3955db4
eunjoo-choi <epcej0020@gmail.com>
2022-09-06 22:00:16 +0900

    API로 가져올 데이터로 만들 Model 생성
    ## 수정 사항
    - API 결과값을 담을 Blog, Image, News Model 구조체 생성

---
+++ SearchApp/SearchApp/Models/Image.swift
@@ -0,0 +1,16 @@
+//
+//  Image.swift
+//  SearchApp
+//
+//  Created by 최은주 on 2022/09/06.
+//
+
+import Foundation
+
+struct Image: Codable {
+    var title: String
+    var link: String
+    var thumbnail: String
+    var sizewidth: String
+    var sizeheight: String
+}
