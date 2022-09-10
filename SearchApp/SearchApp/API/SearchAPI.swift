bf4ae6e8f43c41915385976da647a07d1d289900
eunjoo-choi <epcej0020@gmail.com>
2022-09-10 22:13:55 +0900

    api client key, secret 값 추가
    ## 수정 사항
    - api client key, secret 값 추가
    - api 호출 url을 만드는 로직을 `createURLRequest` 메서드로 분리

--- SearchApp/SearchApp/API/SearchAPI.swift
+++ SearchApp/SearchApp/API/SearchAPI.swift
@@ -20,13 +20,12 @@ class SearchAPI {
         case id = "X-Naver-Client-Id"
         case secret = "X-Naver-Client-Secret"
         
-        // TODO: 추후에 값 발급받아 교체
         var value: String {
             switch self {
             case .id:
-                return ""
+                return "e36FfMQKz631dqTfgjLG"
             case .secret:
-                return ""
+                return "HIqyChuGFA"
             }
         }
     }
@@ -37,18 +36,29 @@ class SearchAPI {
         self.searchType = searchType
     }
     
-    func requestSearchResult(startIndex: Int = 1) {
-        guard let requestURL = URL(string: baseURL + searchType.rawValue) else { return }
+    func requestSearchResult(startIndex: Int = 1, keyword: String) {
+        guard let urlRequest = self.createURLRequest(keyword: keyword) else { return }
         
-        var urlRequest = URLRequest(url: requestURL)
+        URLSession.shared.dataTask(with: urlRequest) { data, response, error in
+            // 결과값 파싱            
+        }.resume()
+    }
+    
+    private func createURLRequest(startIndex: Int = 1, keyword: String) -> URLRequest? {
+        var urlComponents = URLComponents(string: baseURL + searchType.rawValue)
+        
+        let query: URLQueryItem = URLQueryItem(name: "query", value: keyword.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed))
+        let start: URLQueryItem = URLQueryItem(name: "start", value: String(startIndex))
+        urlComponents?.queryItems = [query, start]
+        
+        guard let url = urlComponents?.url else { return nil }
+        
+        var urlRequest = URLRequest(url: url)
         
         urlRequest.httpMethod = "GET"
         urlRequest.addValue(ClientKey.id.value, forHTTPHeaderField: ClientKey.id.rawValue)
         urlRequest.addValue(ClientKey.secret.value, forHTTPHeaderField: ClientKey.secret.rawValue)
         
-        
-        URLSession.shared.dataTask(with: urlRequest) { data, response, error in
-            // 결과값 파싱
-        }.resume()
+        return urlRequest
     }
 }
