9ee45b2dad49f40c6ccb3326f225a7321563feb1
eunjoo-choi <epcej0020@gmail.com>
2022-09-12 18:37:14 +0900

    검색 api 결과 파싱 로직 추가
    ## 수정 사항
    - SearchType을 Model 그룹의 별도 파일로 분리
    - 검색 결과 파싱을 위해 APIResult 구조 추가
    - 각 카테고리 별로 json 파싱 로직 추가
    - 결과 업데이트는 메인 스레드에서 하도록 수정
       > Publishing changes from background threads is not allowed; make sure to publish values from the main thread (via operators like receive(on:)) on model updates.
    
    ## 추후 추가 사항
    - 에러 로직 추가

--- SearchApp/SearchApp/API/SearchAPI.swift
+++ SearchApp/SearchApp/API/SearchAPI.swift
@@ -6,14 +6,13 @@
 //
 
 import Foundation
+import SwiftUI
 
-enum SearchType: String {
-    case blog
-    case news
-    case image
-}
-
-class SearchAPI {
+class SearchAPI: ObservableObject {
+    @Published var blogs: [Blog] = []
+    @Published var news: [News] = []
+    @Published var photos: [Photo] = []
+    
     private let baseURL = "https://openapi.naver.com/v1/search/"
     
     private enum ClientKey: String {
@@ -39,15 +38,36 @@ class SearchAPI {
     func requestSearchResult(startIndex: Int = 1, keyword: String) {
         guard let urlRequest = self.createURLRequest(keyword: keyword) else { return }
         
-        URLSession.shared.dataTask(with: urlRequest) { data, response, error in
-            // 결과값 파싱            
+        URLSession.shared.dataTask(with: urlRequest) {data, response, error in
+            // TODO: 에러 처리
+            
+            guard let data = data else { return }
+
+            let decoder = JSONDecoder()
+            
+            DispatchQueue.main.async {
+                switch self.searchType {
+                case .blog:
+                    if let result = try? decoder.decode(APIResult<Blog>.self, from: data) {
+                        self.blogs.append(contentsOf: result.items)
+                    }
+                case .news:
+                    if let result = try? decoder.decode(APIResult<News>.self, from: data) {
+                        self.news.append(contentsOf: result.items)
+                    }
+                case .image:
+                    if let result = try? decoder.decode(APIResult<Photo>.self, from: data) {
+                        self.photos.append(contentsOf: result.items)
+                    }
+                }
+            }
         }.resume()
     }
     
     private func createURLRequest(startIndex: Int = 1, keyword: String) -> URLRequest? {
         var urlComponents = URLComponents(string: baseURL + searchType.rawValue)
         
-        let query: URLQueryItem = URLQueryItem(name: "query", value: keyword.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed))
+        let query: URLQueryItem = URLQueryItem(name: "query", value: keyword)
         let start: URLQueryItem = URLQueryItem(name: "start", value: String(startIndex))
         urlComponents?.queryItems = [query, start]
         
--- SearchApp/SearchApp/Models/Photo.swift
+++ SearchApp/SearchApp/Models/Photo.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Photo: Codable {
+struct Photo: Codable, Hashable {
     var title: String
     var link: String
     var thumbnail: String
