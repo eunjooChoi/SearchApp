24afab5dd179077848753380c27c725584679a86
eunjoo-choi <epcej0020@gmail.com>
2022-09-29 22:40:08 +0900

    json 데이터에 html 특수 문자가 내려오는 부분 치환 처리
    ## 수정사항
    - String의 extension으로 html을 Encode 된 문자열로 변경하도록 하는 failable initializer 생성
    - API 결과를 파싱할 때 뉴스, 이미지, 블로그의 결과에서 title과 description에 대해 htmlEncodedString을 적용하도록 수정

--- SearchApp/SearchApp/Models/Photo.swift
+++ SearchApp/SearchApp/Models/Photo.swift
@@ -8,9 +8,27 @@
 import Foundation
 
 struct Photo: Codable, Hashable {
+    enum CodingKeys: String, CodingKey {
+        case title, link, thumbnail, sizewidth, sizeheight
+    }
+    
     var title: String
     var link: String
     var thumbnail: String
     var sizewidth: String
     var sizeheight: String
+}
+
+extension Photo {
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        let decodedTitle = try container.decode(String.self, forKey: .title)
+        self.title = String(htmlEncodedString: decodedTitle) ?? ""
+        
+        self.link = try container.decode(String.self, forKey: .link)
+        self.thumbnail = try container.decode(String.self, forKey: .thumbnail)
+        self.sizewidth = try container.decode(String.self, forKey: .sizewidth)
+        self.sizeheight = try container.decode(String.self, forKey: .sizeheight)
+    }
 }
